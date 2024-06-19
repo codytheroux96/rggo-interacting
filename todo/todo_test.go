f5f7571faf431e1eab00c8054adbc6e353a9867d
codytheroux96 <cody.theroux3@gmail.com>
2024-06-18 20:46:41 -0400

    added a test case for the add method

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -1,1 +1,19 @@
-package todo
+package todo_test
+
+import (
+	//"os"
+	"testing"
+
+	"github.com/codytheroux96/rggo-interacting"
+)
+
+func TestAdd(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != "New Task" {
+		t.Errorf("expected %q; got %q instead", taskName, l[0].Task)
+	}
+}
